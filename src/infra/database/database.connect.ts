9a7e5bee216f9d5bc85566f17624d85a43c2fc1f
Everton Moraes <35983284+EvertonLMsilva@users.noreply.github.com>
2024-02-03 13:41:22 -0300

    refactor(connection-db): adjuste connection database

--- src/infra/database/database.connect.ts
+++ src/infra/database/database.connect.ts
@@ -3,11 +3,11 @@ import 'dotenv/config';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: process.env.POSTGRE_HOST,
-  port: parseInt(process.env.POSTGRE_PORT),
-  username: `${process.env.POSTGRE_USER}`,
-  password: `${process.env.POSTGRE_PASSWORD}`,
-  database: process.env.POSTGRE_NAME,
+  host: process.env.POSTGRES_HOST,
+  port: parseInt(process.env.POSTGRES_PORT),
+  username: `${process.env.POSTGRES_USER}`,
+  password: `${process.env.POSTGRES_PASSWORD}`,
+  database: process.env.POSTGRES_DATABASE,
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   synchronize: true,
   autoLoadEntities: true,
